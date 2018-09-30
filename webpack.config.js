92d3b36e3994e14f26d7ebaf532890d493e503a7
Rudyhub <670752970@qq.com>
2018-10-01 06:21:15 +0800

    static path

--- webpack.config.js
+++ webpack.config.js
@@ -10,55 +10,55 @@ distPath = path.join(__dirname, 'dist');
 let {entry, output} = prebuild(distPath);
 
 stats = {
-    all: false,
-    errors: true,
-    errorDetails: true,
-    modules: true,
-    excludeModules(ms) {
-        return !/\/src\/[\s\S]*?\.(vue|js|json)$/i.test(ms);
-    }
+  all: false,
+  errors: true,
+  errorDetails: true,
+  modules: true,
+  excludeModules(ms) {
+    return !/\/src\/[\s\S]*?\.(vue|js|json)$/i.test(ms);
+  }
 };
 
 module.exports = {
-    entry,
-    output,
-    resolve: {
-        extensions: [ '.js', '.vue', '.json' ],
-        alias: { 'vue$': 'vue/dist/vue.esm.js'}
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }, {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            }
+  entry,
+  output,
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {'vue$': 'vue/dist/vue.esm.js'}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new CopyWebpackPlugin([{
-            from: path.join(__dirname, 'static'),
-            to: path.join(__dirname, 'dist')
-        }])
-    ],
-    stats,
-    devServer: {
-        contentBase: distPath,
-        compress: true,
-        port: 8080,
-        clientLogLevel: 'warning',
-        hot: true,
-        stats
-    }
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([{
+      from: path.join(__dirname, 'static'),
+      to: path.join(__dirname, 'dist')
+    }])
+  ],
+  stats,
+  devServer: {
+    contentBase: distPath,
+    compress: true,
+    port: 8080,
+    clientLogLevel: 'warning',
+    hot: true,
+    stats
+  }
 };
